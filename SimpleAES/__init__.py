5c0a6fa6c4e41c86aedecf2e055b55c3b46315dc
jathanism <jathan@gmail.com>
2013-06-16 18:02:17 -0700

    Removed dependency on PyCrypto; Pure Standard Lib now! :)

--- SimpleAES/__init__.py
+++ SimpleAES/__init__.py
@@ -4,7 +4,6 @@ import base64
 import hashlib
 import warnings
 from StringIO import StringIO
-from Crypto.Cipher import AES
 from .version import VERSION
 from .exceptions import EncryptionError, DecryptionError
 
@@ -53,12 +52,8 @@ class SimpleAES(object):
         except:
             raise EncryptionError('Could not encrypt.')
 
-    def decrypt(self, b64_ciphertext, legacy='auto'):
+    def decrypt(self, b64_ciphertext):
         """Decrypts a string using AES-256."""
-        if legacy is True or (legacy == 'auto' and
-                              not b64_ciphertext.startswith('U2Fsd')):
-            return self._legacy_decrypt(b64_ciphertext)
-
         try:
             envvar = hashlib.sha256(_random_noise(16)).hexdigest()
             plaintext = check_output([
@@ -69,40 +64,6 @@ class SimpleAES(object):
             return plaintext
         except:
             raise DecryptionError('Could not decrypt.')
-
-    def _legacy_decrypt(self, b64_ciphertext):
-        """Decrypts a string that's encoded with a SimpleAES version < 1.0.
-        To convert a ciphertext to the new-style algo, use:
-
-            aes = SimpleAES('my secret')
-            aes.convert(legacy_ciphertext)
-        """
-        cipherbytes = base64.b64decode(b64_ciphertext)
-        fin = StringIO(cipherbytes)
-        fout = StringIO()
-
-        key = hashlib.sha256(self._password).digest()
-        chunksize = 64 * 1024
-        try:
-            input_size = struct.unpack('<Q', fin.read(struct.calcsize('Q')))[0]
-            iv = fin.read(16)
-            aes = AES.new(key, AES.MODE_CBC, iv)
-
-            while True:
-                chunk = fin.read(chunksize)
-                if len(chunk) == 0:
-                    break  # done
-                fout.write(aes.decrypt(chunk))
-
-            # truncate any padded random noise
-            fout.truncate(input_size)
-
-            text = fout.getvalue()
-        finally:
-            fin.close()
-            fout.close()
-
-        return text
 
     def base64_encrypt(self, string):
         # Since 1.0, encrypt returns the result base64-encoded already
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    install_requires=['pycrypto'],
+    install_requires=[],
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         #'Development Status :: 1 - Planning',
